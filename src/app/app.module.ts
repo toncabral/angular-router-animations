f5de9a3303a2631c0018b3c22b432d52a2d3a7aa
toncabral <toncabral88@gmail.com>
2020-11-27 16:43:49 -0400

    Configuração inicial do App Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
@@ -9,8 +10,8 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, ContactComponent, AboutComponent ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HomeComponent, ContactComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
